d2fe6904917b6fcc861a9296c9a19a0fc7367324
Gabriel Rodrigues Pacheco <65188122+Gabrija81@users.noreply.github.com>
2023-10-03 01:53:34 -0400

    Update app.py (dearpygui 2023)
    Code update for compatibility with DearPyGUI in 2023.
    Some sections from Mariya's original code were omitted, as I'm still in the learning process.
    Ongoing tasks include themes, as well as enhancements in padding, spacing, and borders.

--- finishedProject/app.py
+++ finishedProject/app.py
@@ -1,63 +1,60 @@
-#DearPyGUI Imports
-from dearpygui.core import *
-from dearpygui.simple import *
+# DearPyGUI Imports
+from dearpygui.dearpygui import *
 
-#functions.py Imports
-from functions import categorize_words, pre_process, predict
+# functions.py Imports
+from functions import pre_process, predict
 
-pred = []
-#button callbak function
-#runs each time when the "Check" button is clicked
-def check_spam(pred):
-    with window("Simple SMS Spam Filter"):
-        if pred == []:
-            #runs only once - the the button is first clicked
-            #and pred[-1] widget doesn't exist
-            add_spacing(count=12)
-            add_separator()
-            add_spacing(count=12)
-        else:
-            #hide prediction widget
-            hide_item(pred[-1])
-        #collect input, pre-process and get prediction
-        input_value = get_value("Input")
-        input_value = pre_process(input_value)
-        pred_text, text_colour = predict(input_value)
-        #store prediction inside the pred list
-        pred.append(pred_text)
-        #display prediction to user
-        add_text(pred[-1], color=text_colour)
 
-#window object settings
-set_main_window_size(540, 720)
-set_global_font_scale(1.25)
-set_theme("Gold")
-set_style_window_padding(30,30)
+# button callbak function
+# runs each time when the "Check" button is clicked
+def check_spam():
+    # collect input
+    input_value = get_value("_Input")
+    print(f"input value:\n\t{input_value}")
 
-with window("Simple SMS Spam Filter", width=520, height=677):
+    # input pre-process and get prediction
+    input_value = pre_process(input_value)
+    pred_text, text_color = predict(input_value)
+
+    # delete last prediction displayed
+    delete_item("del")
+    # display prediction to user
+    add_text(default_value=pred_text, color=text_color, tag="del", parent="win1", before="text pred before this")
+
+# DearPyGUI
+create_context()
+set_global_font_scale(1.25)  # global font scale
+
+# loading logo_spamFilter.png
+width, height, channels, data = load_image("logo_spamFilter.png")
+with texture_registry(show=False):
+    add_static_texture(width=width, height=height, default_value=data, tag="logo")
+
+with window("Simple SMS Spam Filter", width=520, height=677, tag="win1"):
     print("GUI is running...")
-    set_window_pos("Simple SMS Spam Filter", 0, 0)
+    with group(pos=[30, 70]):
+        # inserting logo_spamFilter.png
+        add_image(texture_tag="logo")
+    # height of image + position from image group
+    with group(pos=[30, height+70]):add_separator()
+        add_spacer(height=12)
+        # text instructions
+        add_text("Please Enter an SMS message of your choice to check if it's spam or not.",
+                 color=[232, 163, 33], wrap=460)
+        add_spacer(height=12)
+        # collect input
+        add_input_text(label="Input", tag="_Input", width=415, default_value="type message here!")
+        add_spacer(height=12)
+        # action button
+        add_button(label="Check", callback=check_spam)
+        add_spacer(height=12)
+        add_separator()
+        add_spacer(height=12)
+        add_spacer(tag="text pred before this")
 
-    #image logo
-    add_drawing("logo", width=520, height=290) #create some space for the image
-
-    add_separator()
-    add_spacing(count=12)
-    #text instructions
-    add_text("Please enter an SMS message of your choice to check if it's spam or not",
-    color=[232,163,33])
-    add_spacing(count=12)
-    #collect input
-    add_input_text("Input", width=415, default_value="type message here!")
-    add_spacing(count=12)
-    #action button
-    add_button("Check", callback=lambda x,y:check_spam(pred))
-
-#place the image inside the space
-draw_image("logo", "logo_spamFilter.png", [0, 240])
-
-#IF THE PREVIOUS LINE OF CODE TRIGGERS AN ERRROR TRY
-#draw_image("logo", "logo_spamFilter.png", [0,0], [458,192])
-
+create_viewport(width=540, height=720, x_pos=600, y_pos=256)
+setup_dearpygui()
+show_viewport()
 start_dearpygui()
 print("Bye Bye, GUI")
+destroy_context()
